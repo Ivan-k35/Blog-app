aebcb76311aac5c2c126d934a98f48bba8e07a0f
Ivan Koshkin <ivan.koshkin35@gmail.com>
2023-03-01 12:10:43 +0300

    Updated views.py and blog/urls.py, added PostListView

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
-from django.core.paginator import Paginator
+
+
+class PostListView(ListView):
+    """
+    Alternative post_list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_list(request):
@@ -8,8 +19,14 @@ def post_list(request):
     # Pagination with 3 posts per page
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
-
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page_number is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
