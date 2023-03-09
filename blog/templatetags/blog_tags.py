3f2c5732aa1da7f1abab36bbb77d69d9cb34b076
Ivan Koshkin <ivan.koshkin35@gmail.com>
2023-03-09 16:03:40 +0300

    Added latest_posts.html, updated base.html, blog_tags.py

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 from ..models import Post
 
 register = template.Library()
@@ -7,3 +8,16 @@ register = template.Library()
 @register.simple_tag
 def total_post():
     return Post.published.count()
+
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_post = Post.published.order_by('-publish')[:count]
+    return {'latest_post': latest_post}
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(
+        total_comments=Count('comments')
+    ).order_by('-total_comments')[:count]
