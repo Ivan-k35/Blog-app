c4169d5d33b358e4182f0dfbb8f2b146a7c86a4b
Ivan Koshkin <ivan.koshkin35@gmail.com>
2023-03-10 21:03:02 +0300

    Switched SQLite to PostgreSQL

--- mysite/settings.py
+++ mysite/settings.py
@@ -91,8 +91,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': os.getenv('USER_DB'),
+        'PASSWORD': os.getenv('PASSWORD_DB'),
     }
 }
 
