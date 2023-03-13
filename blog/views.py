131a12ff1c415055f21c08fd97bed5cb5162e6ed
Ivan Koshkin <ivan.koshkin35@gmail.com>
2023-03-13 19:42:10 +0300

    Added search for posts

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,12 @@ from django.core.mail import send_mail
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.db.models import Count
 from django.views.decorators.http import require_POST
+from django.contrib.postgres.search import SearchVector, SearchRank, SearchQuery
 # from django.views.generic import ListView
 
 from taggit.models import Tag
 from .models import Post
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 
 
 # class PostListView(ListView):
@@ -102,3 +103,22 @@ def post_comment(request, post_id):
         comment.save()
     context = {'post': post, 'form': form, 'comment': comment}
     return render(request, 'blog/post/comment.html', context)
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(
+                search=search_vector, rank=SearchRank(search_vector, search_query)
+            ).filter(rank__gte=0.3).order_by('-rank')
+
+    context = {'form': form, 'query': query, 'results': results}
+    return render(request, 'blog/post/search.html', context)
--- mysite/settings.py
+++ mysite/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'django.contrib.postgres',
     'blog.apps.BlogConfig',
     'taggit',
 ]
